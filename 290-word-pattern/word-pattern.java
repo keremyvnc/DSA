868b79f39d5fba0f6cfc04c3d04569f28275ee17
keremyvnc <115942730+keremyvnc@users.noreply.github.com>
2025-12-21 14:51:47 +0300

    Time: 2 ms (16.40%) | Memory: 43 MB (9.92%) - LeetSync

--- 290-word-pattern/word-pattern.java
+++ 290-word-pattern/word-pattern.java
@@ -1,17 +1,22 @@
 class Solution {
+    //keremyvnc
     public boolean wordPattern(String pattern, String s) {
         String[] words = s.trim().split("\\s+");
+        if(words.length != pattern.length()) return false;
+
         HashMap<Character, Integer> charMap = new HashMap<>();
         HashMap<String, Integer> wordMap = new HashMap<>();
-        if(words.length != pattern.length()) return false;
+        
         for(int i = 0; i<pattern.length(); i++){
-            if(!charMap.containsKey(pattern.charAt(i))) {
-                charMap.put(pattern.charAt(i), i);
+            char c = pattern.charAt(i);
+            String w = words[i];
+            if(!charMap.containsKey(c)) {
+                charMap.put(c, i);
             }
-            if(!wordMap.containsKey(words[i])) {
-                wordMap.put(words[i], i);
+            if(!wordMap.containsKey(w)) {
+                wordMap.put(w, i);
             }
-            if(!charMap.get(pattern.charAt(i)).equals(wordMap.get(words[i]))) {
+            if(!charMap.get(c).equals(wordMap.get(w))) {
                 return false;
             }
         }
