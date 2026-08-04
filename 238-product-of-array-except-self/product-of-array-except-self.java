16d75c0a575f50f67fe5e8386d13e7e92166a4ec
keremyvnc <115942730+keremyvnc@users.noreply.github.com>
2026-08-04 22:01:56 +0300

    Time: 2 ms (94.02%) | Memory: 71.8 MB (59.04%) - LeetSync

--- 238-product-of-array-except-self/product-of-array-except-self.java
+++ 238-product-of-array-except-self/product-of-array-except-self.java
@@ -5,7 +5,6 @@ class Solution {
 
         for(int i=1;i<nums.length;i++){
             output[i]=output[i-1]*nums[i-1];
-            System.out.println(output[i]);
         }
         int suffix=1;
         for(int i=nums.length-2;i>=0;i--){
