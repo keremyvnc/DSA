5cb0ba1cfc347eb5e3199ffd10f8a2f21c93f1ae
keremyvnc <115942730+keremyvnc@users.noreply.github.com>
2026-08-31 22:45:01 +0300

    Time: 1 ms (86.59%), Space: 44.4 MB (20.42%) - LeetHub

--- 0078-subsets/0078-subsets.java
+++ 0078-subsets/0078-subsets.java
@@ -1,22 +1,20 @@
 class Solution {
     List<List<Integer>> result = new ArrayList<>();
-    List<Integer> nums;
     public List<List<Integer>> subsets(int[] nums) {
-        this.nums = Arrays.stream(nums).boxed().toList();
-        backtracking(new ArrayList<>(), 0);
+        backtracking(new ArrayList<>(), 0, nums);
         return result;
     }
 
-    void backtracking(List<Integer> solution, int i) {
-        if(i==nums.size()){
+    void backtracking(List<Integer> solution, int i, int[] nums) {
+        if(i==nums.length){
             result.add(new ArrayList<>(solution));
             return;
         }
         
-        solution.add(nums.get(i));
-        backtracking(solution, i+1);
+        solution.add(nums[i]);
+        backtracking(solution, i+1, nums);
         solution.remove(solution.size()-1);
-        backtracking(solution, i+1);
+        backtracking(solution, i+1, nums);
     }
 
 
