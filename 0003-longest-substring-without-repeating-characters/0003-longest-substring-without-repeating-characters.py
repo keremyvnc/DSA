2f1e80f9fb474c42cb1b2d2b2bc4c49a5203022a
keremyvnc <115942730+keremyvnc@users.noreply.github.com>
2026-09-11 00:52:03 +0300

    Time: 235 ms (23.7%), Space: 20 MB (8.58%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.py
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.py
@@ -3,20 +3,15 @@ class Solution:
         leftPointer = 0
         rightPointer = 0
         longest = 0
-        unique_length = 0
         unique_set = set()
-
         while rightPointer < len(s):
-            if s[rightPointer] not in unique_set:
-                unique_set.add(s[rightPointer])
-                rightPointer += 1
-                unique_length += 1
-                longest = max(longest, unique_length)
-            else:
-                while s[rightPointer] in unique_set:
-                    unique_set.discard(s[leftPointer])
+            while s[rightPointer] in unique_set:
+                    unique_set.remove(s[leftPointer])
                     leftPointer +=1
-                    unique_length -= 1
+            unique_set.add(s[rightPointer])
+            rightPointer += 1
+            longest = max(longest, rightPointer - leftPointer)
+
         return longest
 
 
